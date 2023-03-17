8fbbd10e2301a082e84702a76abd5c862947dfee
Jorge Alejandre <88166019+JayySaucee@users.noreply.github.com>
2023-03-17 00:36:00 -0700

    Add files via upload

---
+++ frontend/src/pages/AddExercisePage.js
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+import { useHistory } from "react-router-dom";
+
+export const AddExercisePage = () => {
+
+    const [name, setName]       = useState('');
+    const [reps, setReps]         = useState('');
+    const [weight, setWeight] = useState('');
+    const [unit, setUnit] = useState('');
+    const [date, setDate] = useState('');
+    
+    const history = useHistory();
+
+    const addExercise = async () => {
+        const newExercise = { name, reps, weight, unit, date };
+        const response = await fetch('/exercises', {
+            method: 'post',
+            body: JSON.stringify(newExercise),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        if(response.status === 201){
+            alert("Successfully added the exercise!");
+        } else {
+            alert(`Failed to add exercise, status code = ${response.status}`);
+        }
+        history.push("/");
+    };
+
+
+    return (
+        <>
+        <article>
+            <h2>Add to your Workout Logbook</h2>
+            <p>This is where you can log your workout information.</p>
+            <form onSubmit={(e) => { e.preventDefault();}}>
+                <fieldset>
+                    <legend>Which exercise are you adding?</legend>
+                    <label for="name">Name of Exercise</label>
+                    <input
+                        type="text"
+                        placeholder="Name of Workout"
+                        value={name}
+                        onChange={e => setName(e.target.value)} 
+                        id="name" />
+                    
+                    <label for="reps">Number of Reps</label>
+                    <input
+                        type="number"
+                        value={reps}
+                        placeholder="Number of Reps"
+                        onChange={e => setReps(e.target.value)} 
+                        id="reps" />
+
+                    <label for="weight">Weight</label>
+                    <input
+                        type="number"
+                        placeholder="Amount of Weight"
+                        value={weight}
+                        onChange={e => setWeight(e.target.value)} 
+                        id="weight" />
+
+                    <label for="unit">Unit</label>
+                    <input
+                        type="select"
+                        placeholder="Type of Weight Unit"
+                        value={unit}
+                        onChange={e => setUnit(e.target.value)} 
+                        id="unit" />
+
+                    <label for="date">Date</label>
+                    <input
+                        type="date"
+                        placeholder="Date of Workout"
+                        value={date}
+                        onChange={e => setDate(e.target.value)} 
+                        id="date" />
+
+                    <label for="submit">
+                    <button
+                        type="submit"
+                        onClick={addExercise}
+                        id="submit"
+                    >Add Exercise</button> to the collection</label>
+                </fieldset>
+                </form>
+            </article>
+        </>
+    );
+}
+
+export default AddExercisePage;
